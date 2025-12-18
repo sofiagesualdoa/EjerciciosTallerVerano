05a39a2df31b28f735f927e8a498ab7847b804c0
sofiagesualdoa <sofiagesualdoa@gmail.com>
2025-12-18 17:07:30 -0300

    ejercicios 9 y 10

---
+++ ejercicio10.py
@@ -0,0 +1,11 @@
+numero=1500000
+lista=list(str(numero))
+contador=0
+if lista[-1]!='0':
+    print("No hay ceros al final")
+else:
+    reversed(lista)
+    for elemento in lista:
+        if elemento=='0':
+            contador+=1
+    print(f"Hay {contador} ceros")
---
+++ ejercicio9.py
@@ -0,0 +1,5 @@
+palabra="Sdo"
+if len(palabra) >= 6:
+    print("Longitud válida")
+else:
+    print("Longitud inválida")
