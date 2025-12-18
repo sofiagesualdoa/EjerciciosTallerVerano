5b4ed23d6c363c676ce08c7a74c61d9efded585d
sofiagesualdoa <sofiagesualdoa@gmail.com>
2025-12-18 16:40:30 -0300

    ejercicio 3 corregido

--- ejercicio3.py
+++ ejercicio3.py
@@ -1,6 +1,17 @@
-lista=[5,7,9,"Hola",6,True,14.9,8]
-elemento=4
-index=elemento-1
-print(lista[index::])
+# lista=[5,7,9,"Hola",6,True,14.9,8]
+# elemento=4
+# index=elemento-1
+# print(lista[index::])
 # Martu: Está resuelto pero tratá de pensar cómo hacerlo sin usar slicing; es decir buecando el
 # elemento dentro de la lista por contenido y no por índice. :)
+# corregido: 
+lista=[5,7,9,"Hola",6,True,14.9,8]
+elemento=9
+encontrado = False
+listaNueva=[]
+for objeto in lista:
+    if objeto == elemento:
+        encontrado = True
+    if encontrado:
+        listaNueva.append(objeto)
+print(listaNueva)
