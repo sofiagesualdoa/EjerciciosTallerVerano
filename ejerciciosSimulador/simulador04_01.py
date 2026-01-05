264ca7b23e941449e6f7fad10842e40bb7606e58
sofiagesualdoa <sofiagesualdoa@gmail.com>
2026-01-05 19:58:42 -0300

    simulador ejercicios del número 4

---
+++ ejerciciosSimulador/simulador04_01.py
@@ -0,0 +1,11 @@
+# Leer el valor de uno de los sensores de distancia e imprimirlo 
+# por consola.
+
+from controller import Robot
+
+TIME_STEP=32
+robot=Robot()
+sensor=robot.getDevice("ps1") 
+sensor.enable(TIME_STEP) 
+while robot.step(TIME_STEP) != -1:
+    print(f"Distancia del sensor ps: {sensor.getValue()}")
---
+++ ejerciciosSimulador/simulador04_02.py
@@ -0,0 +1,18 @@
+# Activar más de un sensor de distancia e imprimir 
+# sus valores por consola.
+
+from controller import Robot
+
+TIME_STEP=32
+robot=Robot()
+sensor0=robot.getDevice("ps0") 
+sensor0.enable(TIME_STEP) 
+sensor1=robot.getDevice("ps1") 
+sensor1.enable(TIME_STEP) 
+sensor2=robot.getDevice("ps2") 
+sensor2.enable(TIME_STEP) 
+while robot.step(TIME_STEP) != -1:
+    print(f"Distancia del sensor ps0: {sensor0.getValue()}")
+    print(f"Distancia del sensor ps1: {sensor1.getValue()}")
+    print(f"Distancia del sensor ps2: {sensor2.getValue()}")
+    break
