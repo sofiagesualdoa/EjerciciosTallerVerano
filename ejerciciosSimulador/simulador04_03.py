0ff20ad3b0e849824ba9657738533dbbf6a0e5e1
sofiagesualdoa <sofiagesualdoa@gmail.com>
2026-01-06 01:28:30 -0300

    ejercicios simulador del numero 4

---
+++ ejerciciosSimulador/simulador04_03.py
@@ -0,0 +1,24 @@
+# Avanzar hasta encontrar una pared y frenar.
+
+from controller import Robot
+
+TIME_STEP=32
+MAX_VEL=6.28
+
+robot=Robot()
+izq=robot.getDevice("wheel1 motor")
+izq.setPosition(float("inf"))
+der=robot.getDevice("wheel2 motor")
+der.setPosition(float("inf"))
+ps7 = robot.getDevice("ps7")
+ps7.enable(TIME_STEP)
+ps0 = robot.getDevice("ps0")
+ps0.enable(TIME_STEP)
+
+while robot.step(TIME_STEP) != -1:
+    if ps7.getValue() < 0.05 or ps0.getValue() < 0.05:
+        izq.setVelocity(0)
+        der.setVelocity(0)
+    else:
+        izq.setVelocity(MAX_VEL)
+        der.setVelocity(MAX_VEL)
---
+++ ejerciciosSimulador/simulador04_04.py
@@ -0,0 +1,23 @@
+# Girar hasta detectar el hueco alrededor del robot
+
+from controller import Robot
+
+TIME_STEP=32
+MAX_VEL=6.28
+
+robot=Robot()
+izq=robot.getDevice("wheel1 motor")
+izq.setPosition(float("inf"))
+der=robot.getDevice("wheel2 motor")
+der.setPosition(float("inf"))
+ps6 = robot.getDevice("ps6")
+ps6.enable(TIME_STEP)
+
+
+while robot.step(TIME_STEP) != -1:
+    if ps6.getValue()>0.12:
+        izq.setVelocity(0)
+        der.setVelocity(0)
+    else:
+        izq.setVelocity(MAX_VEL*0.5)
+        der.setVelocity(-MAX_VEL*0.5)
